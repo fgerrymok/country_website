25abe93ce3788fecaaf6d1b63e2201efcf62a7b1
fgerrymok <142370927+fgerrymok@users.noreply.github.com>
2024-05-15 16:21:02 -0700

    Adding hamburger menu functionality

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const menuBtn = document.getElementById("menu-btn");
+const navigationElem = document.getElementById("header-navigation");
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.style.display = "none";
+});
