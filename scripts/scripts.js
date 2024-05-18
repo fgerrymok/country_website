ab71195aa8022196f19a86b3230e31ad46db5ba4
fgerrymok <142370927+fgerrymok@users.noreply.github.com>
2024-05-17 21:55:29 -0700

    Added all pseudo states on main navigation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,9 +3,6 @@
 const menuBtn = document.getElementById("menu-btn");
 const navigationElem = document.getElementById("main-navigation");
 
-// add a button that when clicked will add append a new class to
-
 menuBtn.addEventListener("click", function () {
-  console.log("You clicked the button");
   navigationElem.classList.toggle("toggled");
 });
