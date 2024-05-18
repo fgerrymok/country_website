7c34e78cea246b44e73862de16dbd4ba8e0d130e
fgerrymok <142370927+fgerrymok@users.noreply.github.com>
2024-05-17 21:08:35 -0700

    Added show and hide function to the navigation menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const menuBtn = document.getElementById("menu-btn");
-const navigationElem = document.getElementById("header-navigation");
+const navigationElem = document.getElementById("main-navigation");
+
+// add a button that when clicked will add append a new class to
 
 menuBtn.addEventListener("click", function () {
-  menuBtn.style.display = "none";
+  console.log("You clicked the button");
+  navigationElem.classList.toggle("toggled");
 });
