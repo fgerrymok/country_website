9fd6316f85f2c5edfd1f19d6d51c71ffb357b64c
fgerrymok <142370927+fgerrymok@users.noreply.github.com>
2024-05-22 22:25:21 -0700

    populated the attractions page with placeholder content and functional popups

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,8 +6,12 @@ const menuBtn = document.getElementById("menu-btn");
 const navigationElem = document.getElementById("main-navigation");
 const pinOne = document.getElementById("pin-one-svg");
 const pinTwo = document.getElementById("pin-two-svg");
+const pinThree = document.getElementById("pin-three-svg");
+const pinFour = document.getElementById("pin-four-svg");
 const attractionOne = document.getElementById("monster-tower-article");
 const attractionTwo = document.getElementById("avenue-of-stars-article");
+const attractionThree = document.getElementById("temple-article");
+const attractionFour = document.getElementById("prison-article");
 const closeBtns = document.querySelectorAll(".close-button");
 
 // event listeners
@@ -24,11 +28,21 @@ pinTwo.addEventListener("click", function () {
   attractionTwo.classList.toggle("show-popup");
 });
 
+pinThree.addEventListener("click", function () {
+  attractionThree.classList.toggle("show-popup");
+});
+
+pinFour.addEventListener("click", function () {
+  attractionFour.classList.toggle("show-popup");
+});
+
 closeBtns.forEach(closePopup);
 
 function closePopup(closeBtn) {
   closeBtn.addEventListener("click", function () {
     attractionOne.classList.remove("show-popup");
     attractionTwo.classList.remove("show-popup");
+    attractionThree.classList.remove("show-popup");
+    attractionFour.classList.remove("show-popup");
   });
 }
