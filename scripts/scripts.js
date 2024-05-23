6c6b88ed1ee146eee277fd593e45401c03d5cde9
fgerrymok <142370927+fgerrymok@users.noreply.github.com>
2024-05-22 21:58:32 -0700

    Created a function that will close popups whenever the x is selected

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,10 @@
 const menuBtn = document.getElementById("menu-btn");
 const navigationElem = document.getElementById("main-navigation");
 const pinOne = document.getElementById("pin-one-svg");
-const attractionOne = document.getElementById("monster-tower");
+const pinTwo = document.getElementById("pin-two-svg");
+const attractionOne = document.getElementById("monster-tower-article");
+const attractionTwo = document.getElementById("avenue-of-stars-article");
+const closeBtns = document.querySelectorAll(".close-button");
 
 // event listeners
 
@@ -16,3 +19,16 @@ menuBtn.addEventListener("click", function () {
 pinOne.addEventListener("click", function () {
   attractionOne.classList.toggle("show-popup");
 });
+
+pinTwo.addEventListener("click", function () {
+  attractionTwo.classList.toggle("show-popup");
+});
+
+closeBtns.forEach(closePopup);
+
+function closePopup(closeBtn) {
+  closeBtn.addEventListener("click", function () {
+    attractionOne.classList.remove("show-popup");
+    attractionTwo.classList.remove("show-popup");
+  });
+}
