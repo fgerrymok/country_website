68a4f5b84fe0056f547dc7b77006b7bf38791da0
fgerrymok <142370927+fgerrymok@users.noreply.github.com>
2024-05-22 19:45:24 -0700

    Added svg to map on attractions.html, made svg clickable to show attraction details

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,18 @@
 "use strict";
 
+// constants
+
 const menuBtn = document.getElementById("menu-btn");
 const navigationElem = document.getElementById("main-navigation");
+const pinOne = document.getElementById("pin-one-svg");
+const attractionOne = document.getElementById("monster-tower");
+
+// event listeners
 
 menuBtn.addEventListener("click", function () {
   navigationElem.classList.toggle("toggled");
+});
+
+pinOne.addEventListener("click", function () {
+  attractionOne.classList.toggle("show-popup");
 });
